9ef16fac2b61a938faf976294b82239012af320e
avpimblesr <allan.pimble@gmail.com>
2021-03-12 21:18:43 -0500

    Deleted unnecessary files; created Counter.jsx

---
+++ src/Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <div style={{ backgroundColor: 'lightgray' }}>
+      <h3>{`Count: ${count}`}</h3>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button onClick={decrement}> Decrement</button>
+        <button onClick={increment}> Increment</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
